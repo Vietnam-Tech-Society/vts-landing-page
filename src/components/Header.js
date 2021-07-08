5479a17d63b8fe2cd39f734f6a5567cf3c617c5a
thangplum <thangplum@gmail.com>
2021-07-07 19:01:46 -0500

    Navbar testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import logo from "../assets/img/fulllogo.png";
 
 const Header = () => {
@@ -8,6 +8,12 @@ const Header = () => {
       <Navbar.Brand href="/">
         <img src={logo} alt="logo" />
       </Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/product">Product</Nav.Link>
+        <Nav.Link href="/about">About Us</Nav.Link>
+        <Nav.Link href="/blog">Blog</Nav.Link>
+      </Nav>
     </Navbar>
   );
 }
