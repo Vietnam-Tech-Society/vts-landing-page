fdbe5313f293ec9ee9935cbc75ad75eab77420ec
Nguyen (Vivian) Luu <41164115+nguyenhieuluu@users.noreply.github.com>
2021-07-10 04:16:09 -0400

    header text

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,17 +3,24 @@ import { Col, Container, Row, Image } from 'react-bootstrap'
 import DarkHeader from "../components/DarkHeader";
 import Footer from "../components/Footer"
 import headerimage from "../assets/img/header.png"
+import placeholder from '../assets/img/placeholder.jpg'
 import './Homepage.css'
 
 const Homepage = () => {
     return(
         <div>
             <div className="header">
-                <Image src={headerimage} style={{width: "100%"}}/>
+                <img className="image1" src={headerimage}></img>
                 <DarkHeader></DarkHeader>
-                
-                
+                <div className="header-text">
+                    <h1 className="header-title"> By techies, for techies </h1>
+                    <h5 className="header-subtitle"> A community for Vietnamese tech enthusiasts to share expertise, make meaningful connections, and support each other.</h5>
+                </div>
+
+                <img className="image2" src={placeholder} style={{width: "50%"}} alt=""></img>
+                <div className="absolute-break"></div>
             </div>
+            <div className="header"></div>
             <Footer></Footer>
         </div>
     )
