a9918c79675b405422e276597a39594f66902b84
Nguyen (Vivian) Luu <41164115+nguyenhieuluu@users.noreply.github.com>
2021-07-17 01:20:04 -0400

    button blue

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import './Button.css'
+
+
+const Button = ({ content }) => {
+    return(
+        <div></div>
+    )
+}
+
+export default Button;
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import ProductGrid from "../../components/ProductGrid/ProductGrid";
 import * as constants from '../../components/util/constants';
-import { Col, Container, Row, Image } from 'react-bootstrap'
+import { Col, Container, Row, Image, Button } from 'react-bootstrap'
 import './Product.css'
 
 const Product = () => {
     return (
         <div className="product-page">
-          <Row className="container-gap">
-            <Col xs={12} md={4} className="center" >
-              <h2 className="bold row-margin"> Why mentors choose TechSphere</h2>
-            </Col>
-            <Col xs={12} md={8} >
-              <ProductGrid array={constants.mentorArray} />
+          <Row>
+            <Col>
+            <Button className="button">Sign up for private beta testing &#62;</Button>
             </Col>
           </Row>
           <Row className="container-gap">
@@ -24,8 +21,19 @@ const Product = () => {
             </Col>
           </Row>
           <Row className="container-gap">
-            <Col xs={12} className="center">
-            <h2 className="bold row-margin">Be the first one to experience our product</h2>
+            <Col xs={12} md={4} className="center" >
+              <h2 className="bold row-margin"> Why mentors choose TechSphere</h2>
+            </Col>
+            <Col xs={12} md={8} >
+              <ProductGrid array={constants.mentorArray} />
+            </Col>
+          </Row>
+          <Row className="container-gap">
+            <Col xs={12} xl={8} className="center">
+              <h2 className="bold row-margin">Be the first one to experience our product</h2>
+            </Col>
+            <Col xs={12} xl={4} className="center" >
+            <Button className="button row-margin closer">Sign up for private beta testing &#62;</Button>
             </Col>
           </Row>
           
