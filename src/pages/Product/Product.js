b42cca96a5d6e075381fb3227e13f0b0f422b17a
Nguyen (Vivian) Luu <41164115+nguyenhieuluu@users.noreply.github.com>
2021-07-17 02:15:17 -0400

    done product page

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -3,15 +3,23 @@ import ProductGrid from "../../components/ProductGrid/ProductGrid";
 import * as constants from '../../components/util/constants';
 import { Col, Container, Row, Image, Button } from 'react-bootstrap'
 import './Product.css'
+import placeholder from "../../assets/img/placeholder.jpg"
 
 const Product = () => {
     return (
         <div className="product-page">
-          <Row>
-            <Col>
-            <Button className="button">Sign up for private beta testing &#62;</Button>
+          <Row style={{marginTop:"5vw"}}>
+            <Col sm={12} lg={5} className="center">
+              <h1 className="bold row-margin">Find, Book and Meet Mentors around the world </h1>
+              <h5 className="row-margin" style={{marginTop:"4vw"}} >Get guidance and learn from over 2,261+ mentors in world-class companies with our global community.</h5>
+              <Button className="button row-margin farther ">Sign up for private beta testing &#62;</Button>
+            </Col>
+            <Col sm={12} lg={7} className="img-container">
+              <Image src={placeholder} id="img"/>
             </Col>
           </Row>
+
+
           <Row className="container-gap">
             <Col xs={12} md={4} className="center" >
               <h2 className="bold row-margin"> Why mentors choose TechSphere</h2>
@@ -20,6 +28,7 @@ const Product = () => {
               <ProductGrid array={constants.mentorArray} />
             </Col>
           </Row>
+
           <Row className="container-gap">
             <Col xs={12} md={4} className="center" >
               <h2 className="bold row-margin"> Why mentors choose TechSphere</h2>
@@ -28,6 +37,7 @@ const Product = () => {
               <ProductGrid array={constants.mentorArray} />
             </Col>
           </Row>
+
           <Row className="container-gap">
             <Col xs={12} xl={8} className="center">
               <h2 className="bold row-margin">Be the first one to experience our product</h2>
