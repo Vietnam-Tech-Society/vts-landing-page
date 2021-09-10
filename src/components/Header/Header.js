a64a0d5337b263ae4bcf5a018f161e16e7c1b890
Thang Nguyen <thangplum@gmail.com>
2021-09-09 22:58:23 -0500

    Adding missing links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,6 @@ import { Navbar, Nav } from 'react-bootstrap';
 import darkLogo from "../../assets/img/Logo_full_dark.svg";
 import lightLogo from "../../assets/img/Logo_full_light.svg";
 import './Header.css'
-// variant="dark"
 
 const Header = ({ mode }) => {
   return (
@@ -17,7 +16,7 @@ const Header = ({ mode }) => {
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/product">Product</Nav.Link>
           <Nav.Link href="/about">About Us</Nav.Link>
-          <Nav.Link href="/blog">Blog</Nav.Link>
+          <Nav.Link href="https://vietnamtechsociety.substack.com/">Blog</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -4,6 +4,7 @@ import headerimage from "../../assets/img/header.png"
 import header from '../../assets/img/homepage_header.png'
 import mentorship from '../../assets/img/mentorship.png'
 import './Homepage.css'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
     return (
@@ -13,7 +14,13 @@ const Homepage = () => {
                 <div className="header-text">
                     <h1 className="header-title"> By techies, for techies </h1>
                     <h5> A community for Vietnamese tech enthusiasts to share expertise, make meaningful connections, and support each other.</h5>
-                    <Button style={{backgroundColor:"#14ffd8", color: "#010042", fontWeight: "700", borderStyle: "hidden", padding:"10px"}} className="blue-button"> Tell me more &#62;</Button>
+                    <Link to="/about">
+                        <Button 
+                            style={{backgroundColor:"#14ffd8", color: "#010042", fontWeight: "700", borderStyle: "hidden", padding:"10px"}} 
+                            className="blue-button">
+                                Tell me more &#62;
+                        </Button>
+                    </Link>
                 </div>
                 <img className="image2" src={header} alt=""></img>
             </div>
@@ -23,7 +30,14 @@ const Homepage = () => {
                     <Col xs={12} lg={6}>
                         <h1 className="text-container row-gap" style={{fontWeight: "600"}}> TechSphere </h1>
                         <h4 className="text-container row-gap"> We are a non-profit organization, aspiring to be a top-of-mind organization for Vietnamese people in tech. It is our ambition to be a strong representation of the Vietnamese tech community in the U.S., E.U., Singapore and Vietnam.</h4>
-                        <Button style={{backgroundColor:"#EDF8FF", color: "#010042", fontWeight: "700", borderStyle: "hidden", padding:"10px", marginTop: "7.5vw", marginBottom: "7.5vw"}} className="button-container row-gap"> Start your mentorship here &#62;</Button>
+                        <Link to={{ pathname:"https://techsphere.vietnamtechsociety.org/" }} target="_blank">
+                            <Button
+                                style={{backgroundColor:"#EDF8FF", color: "#010042", fontWeight: "700", borderStyle: "hidden", padding:"10px", marginTop: "7.5vw", marginBottom: "7.5vw"}} 
+                                className="button-container row-gap"
+                            >
+                                Start your mentorship here &#62;
+                            </Button>
+                        </Link>
                     </Col>
                     <Col xs={12} lg={6}>                     
                         <Image id="image-est" src={mentorship} alt="commitment"  />
@@ -91,7 +105,13 @@ const Homepage = () => {
                     </Col>
                     <Col xs={12} lg={6}>                     
                         <h4 className="text-container row-gap">TechSphere is an on-demand mentorship platform that lets you schedule calls with mentors within a few clicks. TechSphere gives mentees access to any suitable mentors seemlessly while giving mentors freedom to set discussion topics and level of commitment</h4>
-                        <Button style={{backgroundColor:"#EDF8FF", color: "#010042", fontWeight: "700", borderStyle: "hidden", padding:"10px"}} className="button-container row-gap"> Read our blogs &#62;</Button>
+                        <Link to={{ pathname:"https://vietnamtechsociety.substack.com/" }} target="_blank">
+                            <Button
+                                style={{backgroundColor:"#EDF8FF", color: "#010042", fontWeight: "700", borderStyle: "hidden", padding:"10px"}} 
+                                className="button-container row-gap">
+                                    Read our blogs &#62;
+                            </Button>
+                        </Link>
                     </Col>
                 </Row>
             </div>
