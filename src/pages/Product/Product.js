01c5bc99af713d5e9f6e63b5ba89be07e38274ec
Nguyen (Vivian) Luu <41164115+nguyenhieuluu@users.noreply.github.com>
2021-10-13 05:24:16 -0400

    fix button link

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ProductGrid from "../../components/ProductGrid/ProductGrid";
 import * as constants from '../../components/util/constants';
 import { Col, Row, Image, Button } from 'react-bootstrap'
@@ -12,7 +13,11 @@ const Product = () => {
             <Col sm={12} xl={6} className="center">
               <h1 className="bold row-margin">Find, Book and Meet Mentors around the world </h1>
               <h5 className="row-margin" style={{marginTop:"4vw"}} >Get guidance and learn from over 2,261+ mentors in world-class companies with our global community.</h5>
-              <Button style={{backgroundColor:"#14ffd8", padding:"10px", color: "#010042", fontWeight: "700", borderStyle: "hidden"}} className="row-margin farther">Sign up for private beta testing &#62;</Button>
+              <Link to={{ pathname: "https://bit.ly/TechSphereSignUp" }} target="_blank">
+                <Button style={{backgroundColor:"#14ffd8", padding:"10px", color: "#010042", fontWeight: "700", borderStyle: "hidden"}} className="row-margin farther">
+                  Sign up for private beta testing &#62;
+                </Button>
+              </Link>
             </Col>
             <Col sm={12} xl={6} className="img-container" >
               <Image src={header} id="img"/>
@@ -44,7 +49,10 @@ const Product = () => {
               <h2 className="bold row-margin" >Be the first one to experience our product</h2>
             </Col>
             <Col xs={12} xl={3} className="center" style={{display:"flex"}}  >
-            <Button style={{backgroundColor:"#14ffd8", padding:"10px", color: "#010042", fontWeight: "700", borderStyle: "hidden"}} className="row-margin closer  center">Signup for beta testing &#62;</Button>
+            <Link to={{ pathname: "https://bit.ly/TechSphereSignUp" }} target="_blank">
+              <Button style={{backgroundColor:"#14ffd8", padding:"10px", color: "#010042", fontWeight: "700", borderStyle: "hidden"}} className="row-margin closer  center">Sign up for private beta testing &#62;
+              </Button>
+            </Link>
             </Col>
           </Row>
           
